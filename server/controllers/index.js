288d6c057f41cff2a99038f6e7d34c26e8d1872f
mKurrels <mjkurrels@gmail.com>
2015-07-25 20:43:44 -0700

    trying to add my client

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,12 +2,11 @@ var models = require('../models');
 var bluebird = require('bluebird');
 
 
-
 module.exports = {
   messages: {
     get: function (req, res) {
+
       models.messages.get(function (result) {
-        console.log('*(&(*&(*&(*&(*&(*&(*&(*&(*&(*& result', typeof JSON.stringify(result));
         res.end(JSON.stringify(result));
       })
     }, // a function which handles a get request for all messages
@@ -17,12 +16,12 @@ module.exports = {
       })
       res.end();
     } // a function which handles posting a message to the database
+
   },
 
   users: {
     // Ditto as above
     get: function (req, res) {
-
 
     },
     post: function (req, res) {
